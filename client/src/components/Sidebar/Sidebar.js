006a1d0d4077cd384e1e11cd3db7c0860dffd0d8
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-05 18:25:21 +0200

    sidebar+posts layout

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import "./Sidebar.css"
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            sidebar
+        </div>
+    )
+}
+
+export default Sidebar
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,17 @@
 import Header from "../../components/Header/Header";
+import Posts from "../../components/Posts/Posts";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import "./Home.css";
 
 const Home = () => {
     return (
-        <div className="home">
+        <>
             <Header />
-            homepage
-        </div>
+            <div className="home">
+                <Posts />
+                <Sidebar />
+            </div>
+        </>
     )
 }
 
