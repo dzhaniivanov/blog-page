43f1fd2ed28013349b5fd7e77c5c71d46c7d4b5e
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-06 10:23:07 +0200

    styling sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -3,7 +3,31 @@ import "./Sidebar.css"
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            sidebar
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img src="https://images.unsplash.com/photo-1638687110777-b3fcdf2f9c57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60" alt="" className="sidebarImg" />
+                <p className="sidebarText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eos necessitatibus commodi quasi distinctio at consequuntur accusantium illum doloribus dolor.</p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
         </div>
     )
 }
