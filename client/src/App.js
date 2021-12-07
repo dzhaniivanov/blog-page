4706a71e40e4a4bb695ec730c41150f3f096e51e
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-07 10:40:17 +0200

    added register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Settings from "./pages/Settings/Settings";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Login />
+      <Register />
     </>
   );
 }
