ce053bebe88868c509d6f055a4175ca389a01d79
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-07 15:06:48 +0200

    create new category added

---
+++ server/routes/categories.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+
+//create category
+router.post("/", async (req, res) => {
+    const newCat = new Category(req.body);
+    try {
+        const savedCat = await newCat.save();
+        res.status(200).json(savedCat);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+module.exports = router;
