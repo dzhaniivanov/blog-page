3cfd663a277aac310cdf6fcff6237e13807f598c
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-07 12:48:26 +0200

    get single user

--- server/routes/users.js
+++ server/routes/users.js
@@ -44,4 +44,15 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+//get user 
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);   
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
